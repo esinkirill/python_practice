7060e1754993c70c595d87851d1a83c461306d5b
Kirill <kirill.esin.2017@gmail.com>
2025-07-01 10:30:45 +0300

    edit proj

--- Tasks/04 Votes/main.py
+++ Tasks/04 Votes/main.py
@@ -2,23 +2,24 @@ import random
 import time
 
 candidates = ["Alice", "Bob", "Charlie"]
-num_votes = 100
-
-votes = [random.choice(candidates) for _ in range(num_votes)]
-
+num_votes = 10
 counts = {}
 
-for idx, vote in enumerate(votes):
+for i in range(num_votes):
+    vote = random.choice(candidates)
     counts.setdefault(vote, 0)
     counts[vote] += 1
-    print(f"Голос №{idx+1}: {vote} + 1 голос")
+    print(f"{vote} + 1 голос")
     time.sleep(0.15)
-print("-" * 30)
+print('-'*30)
 for name, cnt in counts.items():
     print(f"{name}: {cnt} votes")
 
 max_votes = max(counts.values())
+
 winners = [name for name, cnt in counts.items() if cnt == max_votes]
 winner = min(winners)
 
 print(f"\nWinner: {winner}")
+
+
